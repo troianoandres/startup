cfb5f35007db51bd7cb719c3f23e99b26677b207
Troiano Andres <troianoandres@gmail.com>
2015-03-07 11:47:18 -0300

    Upload edit

--- 05-angularjs/exercise/app/modules/movies/controllers/MoviesEditController.js
+++ 05-angularjs/exercise/app/modules/movies/controllers/MoviesEditController.js
@@ -1,19 +1,40 @@
 moviesModule.controller('MoviesEditController', [
   "$scope",
   '$stateParams', 
-  function($scope, $stateParams){
+  "localStorageService",
+  function($scope, $stateParams, localStorageService){
   
     var that = this,
         moviesCache = localStorageService.get("movies");
+    
     that.movie = null;
+    that.movies = [];
 
     if(moviesCache) {
+      that.movies = moviesCache;
+    }    
+
+    if(that.movies) {
       
-      angular.forEach(moviesCache, function(movieObj, key) {
-        if($stateParams.movieIMDB === movieObj.imdb) {
+      angular.forEach(that.movies, function(movieObj, key) {
+        if(parseInt($stateParams.movieIMDB, 10) === movieObj.imdb) {
           that.movie = movieObj;
         }
       });
+
+    }
+
+    that.saveMovie = function() {
+
+      angular.forEach(that.movies, function(movieObj, key) {
+        if(parseInt($stateParams.movieIMDB, 10) === movieObj.imdb) {
+          movieObj = that.movie;
+        }
+      });      
+
+      localStorageService.set("movies", that.movies);
+
+    };
 
   }
 ]);
